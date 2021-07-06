13ddcfa841fb2671fe49efa3ca69bfc5c32ab367
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-07-06 14:25:50 +0300

    re-export

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Component } from 'react';
 import Statistics from './components/Statistics/Statistics';
 // import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+// import Notification from './components/Notification/Notification';
 import FeedbackOptions from './components/FeedbackOptions';
 import Section from './components/Section/Section';
-// import Notification from './components/Notification/Notification';
 import Notification from './components/Notification';
 
 
