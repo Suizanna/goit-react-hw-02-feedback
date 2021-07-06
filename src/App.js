c17fa4c9b91103b0dc52c2a7104386054f4d31e5
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-07-06 14:22:34 +0300

    реэкспорт

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import Statistics from './components/Statistics/Statistics';
-import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+// import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+import FeedbackOptions from './components/FeedbackOptions';
 import Section from './components/Section/Section';
 // import Notification from './components/Notification/Notification';
 import Notification from './components/Notification';
