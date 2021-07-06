708159cd769a7da82242112bcc7f2aa20c62d256
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-07-06 05:59:56 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,31 +1,71 @@
-import './App.css';
 import { Component } from 'react';
+import Statistics from './components/Statistics/Statistics';
+import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+import Section from './components/Section/Section';
+import Notification from './components/Notification/Notification';
+// import Notification from 'components/Notifacation';
 
 
 class App extends Component {
-//внутреннее состояние, которое изменяется методами самого class
+  //внутреннее состояние, которое изменяется методами самого class
+  //в зависимости от state рендерит разметку
   state = {
     good: 0,
     neutral: 0,
     bad: 0
   };
-
-  addFeedback = () => {
-     this.setState( (prevState) => prevState +1) //от предыдущее состояние
-   };
+  //чтобы изменить state у нас есть метод this.setState,который принимает объект или callback ф-цию
+  //setState всегда асинхронный
+  //prevState-параметр ф-ции, может называтся как угодно
+  //в этот параметр аргументом записывается старый state
+  addFeedback = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1 //от предыдущего состояния +1
+    })
+    );
+  };
   //вспомогательные методы //публичное свойство class
-  countTotalFeedback = () =>{};
-  countPositiveFeedbackPercentage = () => { };
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, option) => acc + option, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    return Math.round((good / this.countTotalFeedback()) * 100);
+  };
   
-    render() {
-      return (
-        <div>
-          
-        </div>
-      )
-    }
+  render() {
+    // console.log(this.state);
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.addFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+
+          {total === 0 ? (
+            <Notification message="No feedback given" />
+          ) : (
+                 
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          )}
+        </Section>
+      </>
+    );
+  }
 }
-
-
-
 export default App;
+          
--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,19 +1,33 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import s from './FeedbackOptions.module.css';
+import PropTypes from 'prop-types';
+import s from './FeedbackOptions.module.css';
+
+function FeedbackOptions( {options,  onLeaveFeedback}) {
+    return options.map(option => (
+        <button 
+            type="button"
+            className={s.button}
+            key={option}
+            onClick={()=> onLeaveFeedback(option)}
+        >
+            {option}
+        </button>
+  ))
+}
+FeedbackOptions.propTypes = {
+    options: PropTypes.arrayOf(PropTypes.string.isRequired),
+    onLeaveFeedback: PropTypes.func.isRequired,
+}
+ 
+export default FeedbackOptions;
 
 //из библиотеки React берем експорт React.Component и создаем class
 //у class обязательный метод render(), который возвращаеят только разметку
 
-class Feedback extends React.Component {
-    render() {
-        return (
-            <div className="Container">
-                <button type="button">Good</button>
-            </div>
-        )
-    }
- }
-
-
-export default Feedback;
+// class FeedbackOptions extends React.Component {
+//     render() {
+//         return (    
+//         <button type="button"> {option}</button>       
+//         )
+//     }
+//  }
